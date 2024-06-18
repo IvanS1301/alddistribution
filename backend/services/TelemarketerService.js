f67636bd86e5333412af670b357265c7fadad9b0
IvanS1301 <165673961+IvanS1301@users.noreply.github.com>
2024-06-18 17:12:32 +0800

    Update TelemarketerService.js

--- backend/services/TelemarketerService.js
+++ backend/services/TelemarketerService.js
@@ -3,6 +3,7 @@ const RecentBooking = require('../models/recentBookingModel');
 const Lead = require('../models/leadModel');
 const mongoose = require('mongoose');
 const UserLG = require('../models/userLGModel');
+const Email = require('../models/emailModel');
 
 const getBookedUnitsPerformance = async (req, res) => {
     try {
@@ -18,6 +19,19 @@ const updateBookedUnits = async () => {
     try {
         const allTelemarketers = await UserLG.find({ role: 'Telemarketer' }); // Fetch all telemarketer users
         const performanceData = [];
+
+        // Define all possible types
+        const typeEnum = [
+            "Warehouse", "Restaurant", "Boutiques", "Salon", "Spa",
+            "Manufacturing", "Hotel", "Gym", "Automotive", "Cafe",
+            "Brewery", "Pet Shops", "Laundry", "Clinic"
+        ];
+
+        // Define all possible call dispositions
+        const callDispositionEnum = [
+            "Not Eligible", "Already Installed", "Wrong/Not Working", "Booked", "Residential",
+            "Callback", "Do Not Call", "No Answer", "Not Interested", "Voicemail", "Warm Lead", "Email"
+        ];
 
         await Promise.all(allTelemarketers.map(async (telemarketer) => {
             const telemarketerName = telemarketer.name;
@@ -53,7 +67,7 @@ const updateBookedUnits = async () => {
             // Calculate the number of leads assigned to this telemarketer today
             const assignedDaily = await Lead.countDocuments({
                 assignedTo: telemarketer._id,
-                updatedAt: {
+                Distributed: {
                     $gte: startOfToday,
                     $lt: endOfToday
                 }
@@ -62,11 +76,108 @@ const updateBookedUnits = async () => {
             // Calculate the number of leads assigned to this telemarketer yesterday
             const assignedYesterday = await Lead.countDocuments({
                 assignedTo: telemarketer._id,
-                updatedAt: {
+                Distributed: {
                     $gte: startOfYesterday,
                     $lt: endOfYesterday
                 }
             });
+
+            // Calculate the number of leads tracked by this telemarketer
+            const assignedLeadStatus = await Lead.countDocuments({
+                assignedTo: telemarketer._id,
+                callDisposition: { $exists: true, $ne: null },
+                remarks: { $exists: true, $ne: null }
+            });
+
+            // Calculate the number of leads without callDisposition
+            const assignedLeadsAvailable = await Lead.countDocuments({
+                assignedTo: telemarketer._id,
+                callDisposition: { $exists: false }
+            });
+
+            // Calculate the number of leads tracked by this telemarketer today
+            const leadTracked = await Lead.countDocuments({
+                assignedTo: telemarketer._id,
+                updatedAt: {
+                    $gte: startOfToday,
+                    $lt: endOfToday
+                },
+                callDisposition: { $exists: true, $ne: null },
+                remarks: { $exists: true, $ne: null }
+            });
+
+            // Initialize typesReceived with all types set to 0
+            const typesReceivedObj = typeEnum.reduce((acc, type) => {
+                acc[type] = 0;
+                return acc;
+            }, {});
+
+            // Count the number of assigned leads for each type
+            const typesReceived = await Lead.aggregate([
+                { $match: { assignedTo: telemarketer._id } },
+                { $group: { _id: '$type', count: { $sum: 1 } } }
+            ]);
+
+            // Update the counts in typesReceivedObj
+            typesReceived.forEach(({ _id, count }) => {
+                if (_id) {
+                    typesReceivedObj[_id] = count;
+                }
+            });
+
+            const typesReceivedDailyObj = typeEnum.reduce((acc, type) => {
+                acc[type] = 0;
+                return acc;
+            }, {});
+
+            const typesReceivedDaily = await Lead.aggregate([
+                { $match: { assignedTo: telemarketer._id, updatedAt: { $gte: startOfToday, $lt: endOfToday } } },
+                { $group: { _id: '$type', count: { $sum: 1 } } }
+            ]);
+
+            typesReceivedDaily.forEach(({ _id, count }) => {
+                if (_id) {
+                    typesReceivedDailyObj[_id] = count;
+                }
+            });
+
+            // Initialize callDispositionCreated with all call dispositions set to 0
+            const callDispositionCreatedObj = callDispositionEnum.reduce((acc, disposition) => {
+                acc[disposition] = 0;
+                return acc;
+            }, {});
+
+            // Count the number of leads for each callDisposition created by this telemarketer
+            const callDispositions = await Lead.aggregate([
+                { $match: { assignedTo: telemarketer._id } },
+                { $group: { _id: '$callDisposition', count: { $sum: 1 } } }
+            ]);
+
+            // Update the counts in callDispositionCreatedObj
+            callDispositions.forEach(({ _id, count }) => {
+                if (_id) {
+                    callDispositionCreatedObj[_id] = count;
+                }
+            });
+
+            const callDispositionDailyObj = callDispositionEnum.reduce((acc, disposition) => {
+                acc[disposition] = 0;
+                return acc;
+            }, {});
+
+            const callDispositionsToday = await Lead.aggregate([
+                { $match: { assignedTo: telemarketer._id, updatedAt: { $gte: startOfToday, $lt: endOfToday } } },
+                { $group: { _id: '$callDisposition', count: { $sum: 1 } } }
+            ]);
+
+            callDispositionsToday.forEach(({ _id, count }) => {
+                if (_id) {
+                    callDispositionDailyObj[_id] = count;
+                }
+            });
+
+            // Count the number of emails sent by this telemarketer
+            const emailsSent = await Email.countDocuments({ userLG_id: telemarketer._id });
 
             const bookedUnits = await BookedUnits.findOneAndUpdate(
                 { telemarketerName },
@@ -77,7 +188,15 @@ const updateBookedUnits = async () => {
                     totalBooked: totalBookings,
                     totalAssignedLeads,
                     assignedDaily,
-                    assignedYesterday
+                    assignedYesterday,
+                    assignedLeadStatus,
+                    leadTracked,
+                    typesReceived: typesReceivedObj,
+                    typesReceivedDaily: typesReceivedDailyObj,
+                    callDispositionCreated: callDispositionCreatedObj,
+                    assignedLeadsAvailable,
+                    callDispositionDaily: callDispositionDailyObj,
+                    emailsSent
                 },
                 { new: true, upsert: true }
             );
@@ -94,5 +213,6 @@ const updateBookedUnits = async () => {
         throw error;
     }
 };
+
 
 module.exports = { getBookedUnitsPerformance, updateBookedUnits };
